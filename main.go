4d29632b640747aad1d7f799051f17df9ec83ba3
Kai Wells <support@kaiwells.me>
2021-01-23 09:34:38 -0800

    more robust shell interaction

--- main.go
+++ main.go
@@ -100,11 +100,29 @@ func getInstalledPkgs() (installed []string, err error) {
 
 	var stdout io.ReadCloser
 	stdout, err = cmd.StdoutPipe()
+	defer stdout.Close()
+	if err != nil {
+		return
+	}
+
+	var stderr io.ReadCloser
+	stderr, err = cmd.StderrPipe()
+	defer stderr.Close()
 	if err != nil {
 		return
 	}
 
 	if err = cmd.Start(); err != nil {
+		return
+	}
+
+	var errResp []byte
+	errResp, err = ioutil.ReadAll(stderr)
+	if err != nil {
+		return
+	}
+	if len(errResp) != 0 {
+		err = fmt.Errorf(string(errResp))
 		return
 	}
 
